c89eae8d1d7e97e3fc38f4f49cde77aac78331ba
mois <mose.ns@hotmail.com>
2019-09-10 13:20:46 +0200

    Renommer fichier

--- ExoJs/age.js
+++ ExoJs/age.js
@@ -1,5 +1,4 @@
-<script>
+
     let cetteAnnee = 2019, anneeNaiss = 1975;
     let age = cetteAnnee - anneeNaiss;
     console.log(age);
-</script>
--- ExoJs/index.js
+++ ExoJs/index.js
@@ -1,5 +1,4 @@
-<script>
+
     let nbrEnfant = 1, nomFemme = "Jessica", country = "Canada", metier = "Ux and Research designer";
     document.write("<p>Vous serez: "+metier+" vous habiterez au "+country+", et mariez à "+nomFemme+" avec "+nbrEnfant+" enfant ")
 
-</script>
