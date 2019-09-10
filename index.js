704a102718836e042ad60d897d73bc8603a9eaef
mois <mose.ns@hotmail.com>
2019-09-10 13:28:39 +0200

    renommage fichier

---
+++ index.js
@@ -0,0 +1,13 @@
+
+    var prenom = prompt('Entrez votre prénom svp !');
+    /*Lors d'une initialisation en Js, on ne met pas
+    de guillemets pour les chiffres qui ne sont pas censé être
+    une suite de caractère*/
+
+    var nom = "Griffin";
+    var age = 30;
+
+    var pomme = 10, poire = 5;
+
+    var nbrFruit = pomme + poire;
+    var fruitPerson = nbrFruit / 3;
