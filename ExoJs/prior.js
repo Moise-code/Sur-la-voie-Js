704fc9ca5cd8dacd08498bf530ee1319ac6497f9
mois <mose.ns@hotmail.com>
2019-09-10 14:05:23 +0200

    Exercice sur les priorité

---
+++ ExoJs/prior.js
@@ -0,0 +1,14 @@
+var result = (1 + 2) * 3 + 4 / 2;
+/*Décomposition du calcul (PRIORITE)*/
+
+/*D'abord les parenthèse*/
+3 * 3 + 4 / 2
+
+/*Ensuite la multiplication*/
+9 + 4 / 2
+
+/*La division*/
+9 + 2
+
+/*Et enfin l'addition*/
+11
