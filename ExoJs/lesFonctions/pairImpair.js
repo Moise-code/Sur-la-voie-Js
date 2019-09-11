d80b72f5a40e17a78a60cabc11b1c11aabd25e76
mois <mose.ns@hotmail.com>
2019-09-11 16:37:17 +0200

    Exercices Pair et Impair

---
+++ ExoJs/lesFonctions/pairImpair.js
@@ -0,0 +1,22 @@
+/*
+ Exercice : Pair et impair
+Ecrivez une boucle qui va itérer de 0 à 20.
+Pour chaque itération , il va vérifier si le nombre actuel est pair ou impair
+afficher dans la console par exemple : " 2 est pair "
+*/
+
+/*Itération de 0 à 20*/
+for(let i=1; i<=20;i++){
+    console.log(i);
+    /*Vérifier si le nbr est pair ou impair pour caque itération*/
+    
+    if(i % 2 === 0){
+        console.log(i + " " + " est pair");
+    }else{
+        console.log(i + " "+ "est Impair");
+    }
+    
+    
+/*nbr est impair s'il reste 1 lors du modulo. 
+    Est pair s'il reste 0*/
+}
