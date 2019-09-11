5939f414f8d16d8de91abec63d5e482acd416165
mois <mose.ns@hotmail.com>
2019-09-11 16:39:17 +0200

    petite modif

--- ExoJs/lesFonctions/pairImpair.js
+++ ExoJs/lesFonctions/pairImpair.js
@@ -9,7 +9,8 @@ afficher dans la console par exemple : " 2 est pair "
 for(let i=1; i<=20;i++){
     console.log(i);
     /*Vérifier si le nbr est pair ou impair pour caque itération*/
-    
+    /*nbr est impair s'il reste 1 lors du modulo. 
+    Est pair s'il reste 0*/
     if(i % 2 === 0){
         console.log(i + " " + " est pair");
     }else{
@@ -17,6 +18,5 @@ for(let i=1; i<=20;i++){
     }
     
     
-/*nbr est impair s'il reste 1 lors du modulo. 
-    Est pair s'il reste 0*/
+
 }
