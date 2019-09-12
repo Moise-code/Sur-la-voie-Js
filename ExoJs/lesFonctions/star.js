19ae4c230d52ac31bab77deb7880610c830d1051
mois <mose.ns@hotmail.com>
2019-09-12 10:34:45 +0200

    Exercice pyramid

---
+++ ExoJs/lesFonctions/star.js
@@ -0,0 +1,23 @@
+/*
+Exercice : pyramide
+Écrire une boucle qui va dessiner dans 
+a console (ligne par ligne) le résulat suivant
+                *
+                **
+                ***
+                ****
+                *****
+*/
+/*Boucle for qui va à chaque itération déssiner une étoile ( dans une balise p: pour mettre à la ligne)*/
+
+
+for(let i=1; i<=5;i++){
+    for(let j=1; j<=i; j++){
+        document.write("*&nbsp;&nbsp;&nbsp;");   
+    }
+    document.write("<br>")
+}
+
+
+
+
